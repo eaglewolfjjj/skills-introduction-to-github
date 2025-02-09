dd60c8f67e3fc29d924ab6dc6c7ddf68d29cf209
eaglewolfjjj <edelweiss1998@gmail.com>
2025-02-09 12:39:54 +0800

    Add files via upload

---
+++ 查询-精算报表类.sql
@@ -0,0 +1,6 @@
+SELECT sum_date,SUM(year_prem),SUM(ftp_prem),SUM(commission_amnt),SUM(manage_fee) FROM dbo.sumsec_prem
+WHERE i_info_appl_branch='000000'
+GROUP BY sum_date
+
+SELECT * FROM dbo.vw_sumsec_yfprofit
+WHERE i_info_appl_branch='000000'
